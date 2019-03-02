ab0b22b2c2288b2e187e00535801cc2200f2ff40
root user <abf@rosalinux.ru>
2019-03-02 18:41:24 +0300

    change SAVE_TO_PLATFORM to BUILD_FOR_PLATFORM to prevent issues with personal repos

--- scripts/mdv/build.sh
+++ scripts/mdv/build.sh
@@ -13,7 +13,7 @@ testing="$TESTING"
 id="$ID"
 file_store_base='http://file-store.rosalinux.ru'
 # save_to_platform - main or personal platform
-save_to_platform="$SAVE_TO_PLATFORM"
+save_to_platform="$BUILD_FOR_PLATFORM"
 # build_for_platform - only main platform
 build_for_platform="$BUILD_FOR_PLATFORM"
 regenerate_metadata="$REGENERATE_METADATA"
